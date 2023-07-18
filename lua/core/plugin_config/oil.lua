5fb9965e22726cd20a86cb9e24bf3405fbdf1dd5
Masnun Siam <siam.cse.bsmrstu@gmail.com>
2023-07-18 14:16:51 +0600

    changed oil keymaps

--- lua/core/plugin_config/oil.lua
+++ lua/core/plugin_config/oil.lua
@@ -1,4 +1,10 @@
-require("oil").setup()
+require("oil").setup({
+  keymaps = {
+    ["<S-h>"] = "actions.parent",
+    ["<S-l>"] = "actions.select",
+    ["q"] = "actions.close",
+  },
+})
 
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 vim.keymap.set("n", "sf", require("oil").open, { desc = "Open parent directory" })
